779976026750bea8cfe0cd86bf57db8ef1980b6e
Mick Rasmussen <49582401+mickras@users.noreply.github.com>
2020-02-10 12:00:48 +0100

    Update

--- processrobotapidoc/api_project.js
+++ processrobotapidoc/api_project.js
@@ -4,12 +4,16 @@ define({
   "description": "Simple API for ProcessRobot (https://www.softomotive.com/processrobot/), based on Flask and Flask Restful. The back-end logic for the API is handled by the class LogHandler.",
   "title": "Processrobot api documentation",
   "url": "https://localhost",
+  "footer": {
+    "title": "Questions?",
+    "content": "<h2>Contact:</h2>\n<ul>\n<li>GitHub repository: <a href=\"https://github.com/mickras/ProcessRobotAPI\">https://github.com/mickras/ProcessRobotAPI</a></li>\n<li>Developer: Mick Rasmussen, mira19@guldborgsund.dk</li>\n</ul>\n"
+  },
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-02-10T10:45:47.510Z",
+    "time": "2020-02-10T10:58:58.593Z",
     "url": "http://apidocjs.com",
     "version": "0.20.0"
   }
